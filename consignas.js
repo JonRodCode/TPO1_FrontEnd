99a6887803e6863fc44cd0b16f572af7fcb604a8
Jonatan Alexis Rodriguez <jonatanr72@gmail.com>
2025-11-10 21:22:40 -0300

    Comenzando tp, 1er ejercicio

---
+++ consignas.js
@@ -0,0 +1,16 @@
+/*
+ 1. Calcular el área de un rectángulo
+ Escribe una función que reciba la longitud y el ancho de un rectángulo y devuelva su área.
+ 
+ La funcion no debe devolver un  console.log() sino retornar un valor, 
+ llamaremos a la funcion dentro de un console.log() .
+
+ La idea de esto es comprender que algunas funciones retornan un valor y otras simplemente hacen otras cosas,
+ como imprimir un mensaje por consola.
+ 
+ Las que retornan algo, deberán incluir la palabra clave return .
+
+
+ // Longitud = ancho x alto
+ console.log(calcularAreaRectangulo(5, 3)); // Resultado: 15
+*/ 
---
+++ soluciones.js
@@ -0,0 +1,7 @@
+// 1. Calcular el área de un rectángulo
+
+function areaRectangulo(longitud, ancho){
+    return longitud * ancho;
+}
+
+console.log(areaRectangulo(3,6));
