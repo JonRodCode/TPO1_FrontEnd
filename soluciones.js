a07c96c09db0056a2f7118b4d8f65db2f1529adf
Jonatan Alexis Rodriguez <jonatanr72@gmail.com>
2025-11-11 20:36:51 -0300

    2do y 3er ejercicio

--- soluciones.js
+++ soluciones.js
@@ -5,3 +5,26 @@ function areaRectangulo(longitud, ancho){
 }
 
 console.log(areaRectangulo(3,6));
+
+
+// 2. Contar palabras en una cadena
+
+function contadorDePalabras(texto) {
+    const arrayDePalabras = texto.split(" ");
+    return arrayDePalabras.length;
+}
+
+console.log(contadorDePalabras("¿Cuantas palabras hay en este texto? Pues ocho."));
+
+// 3. Contar vocales en una cadena
+
+function contarVocales(texto){
+    const vocales = "aeiou";
+    const arrayDeTexto = texto.split('');
+    const soloVocales = arrayDeTexto.filter((letra) =>{
+        return vocales.includes(letra);
+    });
+    return soloVocales.length;
+}
+
+console.log(contarVocales("Este es un texto que tiene diecinueve vocales."));
