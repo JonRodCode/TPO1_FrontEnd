c12cb9e7f7723ecbdb35aa650580bfb05dc17b28
Jonatan Alexis Rodriguez <jonatanr72@gmail.com>
2025-11-11 21:12:26 -0300

    4to ejercicio y arreglo del 3ro

--- soluciones.js
+++ soluciones.js
@@ -16,15 +16,32 @@ function contadorDePalabras(texto) {
 
 console.log(contadorDePalabras("¿Cuantas palabras hay en este texto? Pues ocho."));
 
+
 // 3. Contar vocales en una cadena
 
 function contarVocales(texto){
     const vocales = "aeiou";
-    const arrayDeTexto = texto.split('');
+    const arrayDeTexto = texto.toLowerCase().split('');
     const soloVocales = arrayDeTexto.filter((letra) =>{
         return vocales.includes(letra);
     });
     return soloVocales.length;
 }
 
-console.log(contarVocales("Este es un texto que tiene diecinueve vocales."));
+console.log(contarVocales("Este es un texto que deberia tener veintisiete vocales, y las tiene."));
+
+
+// 4. Encontrar el palíndromo
+
+function esPalindromo(texto) {
+    const arrayTexto = texto.toLowerCase().split('');
+    for (var i = 0; i < arrayTexto.length; i++){
+        let indiceInvertido = texto.length -1 - i;
+        if (arrayTexto[i] != arrayTexto[indiceInvertido]){
+            return false;
+        }
+    }
+    return true;
+}
+console.log(esPalindromo("Muy complicado escribir un texto que sea palindromo asi que:"));
+console.log(esPalindromo("Espejo, espejito dime quien es el mas bonito ... soy yo? : ?oy yos ... otinob sam le se neiuq emid otijepse ,ojepsE"));
